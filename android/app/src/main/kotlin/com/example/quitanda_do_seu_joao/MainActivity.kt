68cc46f3228b26be99e02ff8cec301fa3fb3f120
thiago.rocha <thiago.rocha@cwi.com.br>
2021-05-30 00:29:35 -0300

    agora vai

--- android/app/src/main/kotlin/com/example/quitanda_do_seu_joao/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.quitanda_do_seu_joao
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -16,8 +16,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "175142adfa43a55577824e254fdb7f0f",
-"/": "175142adfa43a55577824e254fdb7f0f",
+"index.html": "de8ac41ad0fea50222988ad404b4f159",
+"/": "de8ac41ad0fea50222988ad404b4f159",
 "main.dart.js": "4937170b26e352fa0794dc1d64c94d2f",
 "manifest.json": "74f187b5159348f2b9e3301cd6a077b4",
 "version.json": "53617b0083b60da4f39006bd46c0142a"
